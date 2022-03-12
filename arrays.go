6d562caad9e31be15b4a5d0fde3283d5c64b836a
alexey.blokhin <alexey.blokhin@gs-labs.ru>
2022-03-12 12:31:09 +0300

    arrays: stock profit maximization

--- arrays.go
+++ arrays.go
@@ -15,3 +15,36 @@ func RemoveDuplicates(nums []int) int {
 	}
 	return k
 }
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+// Find and return the maximum profit you can achieve. 0 <= prices[i] <= 10^4
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+func MaxProfit(prices []int) int {
+	var (
+		profit int
+		bought int = 1e6 // all prices are lower
+	)
+	// buy on valley, sell on peak
+	for i := range prices {
+		if prices[i] < bought {
+			bought = prices[i]
+		} else if bought < prices[i] && (i == len(prices)-1 || prices[i+1] < prices[i]) {
+			profit += prices[i] - bought
+			bought = 1e6 // just for correct compare operation
+		}
+
+	}
+	return profit
+}
+
+// another realization, buy/sell stocks each day the cost increases
+func MaxProfit2(prices []int) int {
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
--- arrays_test.go
+++ arrays_test.go
@@ -24,7 +24,41 @@ func TestRemoveDuplicates(t *testing.T) {
 			expect:     []int{1, 2, 3, 4, 5},
 		},
 	} {
+		// remove duplicates in place and don't change slice size
 		i := RemoveDuplicates(test.sortedList)
 		assert.Equal(t, test.expect, test.sortedList[:i])
+	}
+}
+
+func TestMaxProfit(t *testing.T) {
+	tests := []struct {
+		prices []int
+		expect int
+	}{
+		{
+			prices: []int{7, 1, 5, 3, 6, 4},
+			expect: 7,
+			// Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+			// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+			// Total profit is 4 + 3 = 7.
+		},
+		{
+			prices: []int{1, 2, 3, 4, 5},
+			expect: 4,
+			// Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+			// Total profit is 4.
+		},
+		{
+			prices: []int{7, 6, 4, 3, 1},
+			expect: 0,
+			// There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+		},
+		{
+			prices: []int{3, 3},
+			expect: 0,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expect, MaxProfit(test.prices))
 	}
 }
