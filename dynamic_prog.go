ab4aa6da586084d283c355c9c65fc0aee1629d03
alexey.blokhin <alexey.blokhin@gs-labs.ru>
2022-05-04 08:27:44 +0300

    dynamic_prog: max subarray and house robber

--- dynamic_prog.go
+++ dynamic_prog.go
@@ -30,3 +30,42 @@ func maxProfitAtAll(prices []int) int {
 	}
 	return profit
 }
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// A subarray is a contiguous part of an array.
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/566/
+func maxSubArray(nums []int) int {
+	sum, maxSum := 0, -math.MaxInt32
+	for _, num := range nums {
+		sum += num
+		if sum > maxSum {
+			maxSum = sum
+		}
+		if sum < 0 {
+			// it can only get worse
+			sum = 0
+		}
+	}
+	return maxSum
+}
+
+// You are a professional robber planning to rob houses along a street.
+// Each house has a certain amount of money stashed, the only constraint stopping
+// you from robbing each of them is that adjacent houses have security systems connected
+// and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given an integer array nums representing the amount of money of each house,
+// return the maximum amount of money you can rob tonight without alerting the police.
+func rob(nums []int) int {
+	var rob1, rob2 int
+	for _, n := range nums {
+		rob1, rob2 = rob2, max(n+rob1, rob2)
+	}
+	return rob2
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- dynamic_prog_test.go
+++ dynamic_prog_test.go
@@ -32,3 +32,33 @@ func TestMaxProfitAtAll(t *testing.T) {
 		assert.Equal(t, test.expect, maxProfitAtAll(test.prices))
 	}
 }
+
+func TestMaxSubArray(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		expect int
+	}{
+		// [4,-1,2,1] has the largest sum = 6
+		{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}, expect: 6},
+		{nums: []int{5, 4, -1, 7, 8}, expect: 23},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expect, maxSubArray(test.nums))
+	}
+}
+
+func TestRob(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		expect int
+	}{
+		{nums: []int{1, 2, 3, 1}, expect: 4},
+		{nums: []int{2, 7, 9, 3, 1}, expect: 12},
+		{nums: []int{2, 1, 1, 2}, expect: 4},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expect, rob(test.nums))
+	}
+}
