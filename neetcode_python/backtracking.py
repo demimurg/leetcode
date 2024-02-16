b16502da1fe36e87f252802af682752b3843cf1c
aleksey.blokhin <alekblokhin@ozon.ru>
2024-02-16 20:57:38 +0300

    backtracking: find all nums combinations for target

--- neetcode_python/backtracking.py
+++ neetcode_python/backtracking.py
@@ -19,6 +19,38 @@ def subsets(nums: List[int]) -> List[List[int]]:
     return subs
 
 
+def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
+    """
+    Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations
+    of candidates where the chosen numbers sum to target. The same number may be chosen from candidates an unlimited
+    number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+    [MEDIUM] https://leetcode.com/problems/combination-sum/
+
+    >>> combination_sum([2, 3, 6, 7], 7)
+    [[2, 2, 3], [7]]
+    >>> combination_sum([2, 3, 5], 8)
+    [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
+    >>> combination_sum([2], 1)
+    []
+    """
+    result = []
+
+    def dfs(nums: List[int], nums_sum: int, i: int):
+        if i == len(candidates) or nums_sum > target:
+            return
+        if nums_sum == target:
+            result.append(nums.copy())
+            return
+
+        nums.append(candidates[i])
+        dfs(nums, nums_sum + candidates[i], i)
+        nums.pop()
+        dfs(nums, nums_sum, i + 1)
+
+    dfs([], 0, 0)
+    return result
+
+
 if __name__ == "__main__":
     import doctest
 
