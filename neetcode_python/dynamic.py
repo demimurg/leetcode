65fec8f9f5955a8cce6b6de7ed5cf4b851ad9cd4
demimurg <blohinalexej@icloud.com>
2024-04-01 12:45:50 -0700

    dynamic: min cost climbing

--- neetcode_python/dynamic.py
+++ neetcode_python/dynamic.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def climb_stairs(n: int) -> int:
     """
     You are climbing a staircase with n steps to reach the top. Each time you can climb 1 or 2 steps. 
@@ -16,6 +19,25 @@ def climb_stairs(n: int) -> int:
     for _ in range(n - 2):
         prelast, last = last, prelast + last
     return last
+
+
+def min_cost_climbing_stairs(cost: List[int]) -> int:
+    """
+    Given a staircase where the ith step has a non-negative cost associated with it represented by the array cost[],
+    we can climb one or two steps at a time. The goal is to find the minimum cost to reach the top of the staircase
+    starting from either the first step or the second step. We pay the cost of a step before climbing it. After paying,
+    we can either climb one step or two steps. The top of the staircase is beyond the last step.
+    [EASY] https://leetcode.com/problems/min-cost-climbing-stairs/
+
+    >>> min_cost_climbing_stairs([10, 15, 20])
+    15
+    >>> min_cost_climbing_stairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
+    6
+    """
+    a, b = 0, 0  # cost for prelast and last step
+    for i in range(2, len(cost) + 1):
+        a, b = b, min(a + cost[i - 2], b + cost[i - 1])
+    return b
 
 
 if __name__ == "__main__":
