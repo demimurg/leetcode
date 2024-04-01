e45a214b0f24b244b91bc737125f9a422941086d
demimurg <blohinalexej@icloud.com>
2024-04-01 13:32:41 -0700

    dynamic: professional robber

--- neetcode_python/dynamic.py
+++ neetcode_python/dynamic.py
@@ -40,6 +40,29 @@ def min_cost_climbing_stairs(cost: List[int]) -> int:
     return b
 
 
+def rob(nums: List[int]) -> int:
+    """
+    You are a professional robber planning to rob houses along a street. Each house has a
+    certain amount of money stashed, the only constraint stopping you from robbing each of
+    them is that adjacent houses have security systems connected and it will automatically
+    contact the police if two adjacent houses were broken into on the same night. Given an
+    integer array nums representing the amount of money of each house, return the maximum
+    amount of money you can rob tonight without alerting the police.
+    [MEDIUM] https://leetcode.com/problems/house-robber/
+
+    >>> rob([1,2,3,1])
+    4
+    >>> rob([2,7,9,3,1])
+    12
+    >>> rob([2,1,1,2])
+    4
+    """
+    prelast, last = 0, 0
+    for n in nums:
+        prelast, last = last, max(prelast + n, last)
+    return last
+
+
 if __name__ == "__main__":
     import doctest
 
