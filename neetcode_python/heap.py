2aa83406417e093323bfebf3f2e941afa0467599
demimurg <blohinalexej@icloud.com>
2024-05-14 15:07:48 -0700

    heap: get k closest points

--- neetcode_python/heap.py
+++ neetcode_python/heap.py
@@ -1,15 +1,18 @@
 import heapq
+import math
 from typing import *
 
 
 class Heap:
-    def __init__(self, is_max=True):
+    def __init__(self, is_max=True, lis=[]):
         self.storage = []
         # x before y, if x biggen than y for max_heap
         self.before = lambda x, y: x > y
         if not is_max:
             # x before y, if x less than y for min_heap
             self.before = lambda x, y: x < y
+        for val in lis:
+            self.add(val)
 
     def __len__(self):
         return len(self.storage)
@@ -97,6 +100,29 @@ def last_stone_weight(stones: List[int]) -> int:
         heapq.heappush(stones, -1 * (a - b))
 
     return -stones.pop() if len(stones) > 0 else 0
+
+
+def k_closest(points: List[List[int]], k: int) -> List[List[int]]:
+    """
+    Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k,
+    return the k closest points to the origin (0, 0). The distance between two points on the X-Y plane is the
+    Euclidean distance (i.e., sqrt((x1 - x2)^2 + (y1 - y2)^2)). The answer is guaranteed to be unique and may
+    be returned in any order.
+    [MEDIUM] https://leetcode.com/problems/k-closest-points-to-origin/
+
+    >>> k_closest([[1,3], [-2,2]], 1)
+    [[-2, 2]]
+    >>> k_closest([[0,1],[1,0]], 2)
+    [[0,1], [1,0]]
+    """
+    distances = [(math.sqrt(x ** 2 + y ** 2), x, y) for x, y in points]
+    heapq.heapify(distances)
+
+    k_points = [[0, 0] for _ in range(k)]
+    for i in range(k):
+        closest_dist, x, y = heapq.heappop(distances)
+        k_points[i] = ([x, y])
+    return k_points
 
 
 if __name__ == "__main__":
