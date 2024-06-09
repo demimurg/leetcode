4fc58e8c477617c3a205ff6af63ed9a2b6cc902a
demimurg <blohinalexej@icloud.com>
2024-06-09 14:42:12 -0700

    graph: network delay time

--- neetcode_python/graph_advanced.py
+++ neetcode_python/graph_advanced.py
@@ -1,5 +1,6 @@
 import heapq
 from typing import *
+from collections import *
 
 
 def min_cost_connect_points(points: List[List[int]]) -> int:
@@ -36,6 +37,47 @@ def min_cost_connect_points(points: List[List[int]]) -> int:
         for j in to_visit:
             heapq.heappush(edges, (manh_dist(i, j), j))
     return cost
+
+
+def network_delay_time(times: List[List[int]], n: int, k: int) -> int:
+    """
+    You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed
+    edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a
+    signal to travel from source to target.
+
+    We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal.
+    If it is impossible for all the n nodes to receive the signal, return -1.
+
+    [MEDIUM] https://leetcode.com/problems/network-delay-time/
+
+    >>> network_delay_time([[2,1,1],[2,3,1],[3,4,1]], 4, 2)
+    2
+    >>> network_delay_time([[1,2,1]], 2, 1)
+    1
+    >>> network_delay_time([[1,2,1]], 2, 2)
+    -1
+    """
+    delays = {}
+    for (a, b, delay) in times:
+        if a not in delays:
+            delays[a] = []
+        delays[a].append([b, delay])
+
+    paths = [[0, k]]  # complete delay to achive node
+    to_visit = set(i for i in range(1, n + 1))
+    network_delay = 0
+    while paths and to_visit:
+        delay, node = heapq.heappop(paths)
+        if node not in to_visit:
+            continue
+        to_visit.remove(node)
+        network_delay = max(network_delay, delay)
+
+        for child_node, child_delay in delays.get(node, []):
+            if child_node in to_visit:
+                heapq.heappush(paths, [delay + child_delay, child_node])
+
+    return network_delay if len(to_visit) == 0 else -1
 
 
 if __name__ == "__main__":
