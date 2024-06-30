bfd21ab80c7fa52534baf66e7623c5d49f9614a2
demimurg <blohinalexej@icloud.com>
2024-06-29 22:57:52 -0700

    dynamic: is interleave string

--- neetcode_python/dyamic_2d.py
+++ neetcode_python/dyamic_2d.py
@@ -204,6 +204,42 @@ def find_target_sum_ways_bruteforce(nums: List[int], target: int) -> int:
     return dfs(0, 0)
 
 
+def is_interleave(s1: str, s2: str, s3: str) -> bool:
+    """
+    Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+    An interleaving of two strings s and t is a configuration where s and t are divided into n and m
+    substrings respectively, such that:
+
+    s = s1 + s2 + ... + sn
+    t = t1 + t2 + ... + tm
+    [n − m] <= 1
+
+    The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+
+    Note: a + b is the concatenation of strings a and b.
+
+    [MEDIUM] https://leetcode.com/problems/interleaving-string/
+
+    >>> is_interleave('aabcc', 'dbbca', 'aadbbcbcac')
+    True
+    >>> is_interleave('aabcc', 'dbbca', 'aadbbbaccc')
+    False
+    >>> is_interleave('', '', '')
+    True
+    """
+    if len(s1) + len(s2) != len(s3):
+        return False
+
+    dp = [False] * (len(s2) + 1)
+    for i in range(len(s1) + 1):
+        for j in range(len(s2) + 1):
+            dp[j] = i > 0 and dp[j] and s1[i - 1] == s3[i + j - 1] or \
+                    j > 0 and dp[j - 1] and s2[j - 1] == s3[i + j - 1] or \
+                    i == 0 and j == 0
+
+    return dp[-1]
+
+
 if __name__ == "__main__":
     import doctest
 
