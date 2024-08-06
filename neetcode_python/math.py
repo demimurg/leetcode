cd2ed5edf5dd86f8feaa8c14725fbe0629d3a8e1
demimurg <blohinalexej@icloud.com>
2024-08-06 09:45:46 +0300

    math: traverse matrix in spiral

--- neetcode_python/math.py
+++ neetcode_python/math.py
@@ -1,3 +1,4 @@
+from select import select
 from typing import *
 
 
@@ -8,10 +9,8 @@ def rotate_image(matrix: List[List[int]]) -> None:
     DO NOT allocate another 2D matrix and do the rotation.
     [MEDIUM] https://leetcode.com/problems/rotate-image/
 
-    >>> rotate_image([[1,2,3],[4,5,6],[7,8,9]])
-    [[7,4,1],[8,5,2],[9,6,3]]
-    >>> rotate_image([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
-    [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    >>> rotate_image([[1,2,3],[4,5,6],[7,8,9]]) # [[7,4,1],[8,5,2],[9,6,3]]
+    >>> rotate_image([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]) # [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
     """
     last = len(matrix) - 1
     for i in range(len(matrix) // 2):
@@ -20,6 +19,41 @@ def rotate_image(matrix: List[List[int]]) -> None:
             temp, matrix[last - i][last - j] = matrix[last - i][last - j], temp  # right to bottom
             temp, matrix[last - j][i] = matrix[last - j][i], temp  # bottom to left
             matrix[i][j] = temp  # left to up
+
+
+def spiral_matrix(matrix: List[List[int]]) -> List[int]:
+    """
+    Given an m x n matrix, return all elements of the matrix in spiral order.
+    [MEDIUM] https://leetcode.com/problems/spiral-matrix/
+
+    >>> spiral_matrix([[1,2,3],[4,5,6],[7,8,9]])
+    [1, 2, 3, 6, 9, 8, 7, 4, 5]
+    >>> spiral_matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+    """
+    right, down, left, up = (0, 1), (1, 0), (0, -1), (-1, 0)
+    i, j, step = 0, 0, right
+    i_min, i_max, j_min, j_max = 1, len(matrix) - 1, 0, len(matrix[0]) - 1
+
+    vals = []
+    for _ in range(len(matrix) * len(matrix[0])):
+        if step == right and j + step[1] > j_max:
+            step = down
+            j_max -= 1
+        elif step == down and i + step[0] > i_max:
+            step = left
+            i_max -= 1
+        elif step == left and j + step[1] < j_min:
+            step = up
+            j_min += 1
+        elif step == up and i + step[0] < i_min:
+            step = right
+            i_min += 1
+
+        vals.append(matrix[i][j])
+        i, j = i + step[0], j + step[1]
+
+    return vals
 
 
 if __name__ == "__main__":
